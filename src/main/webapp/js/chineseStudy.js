8448fef12f25a4b201dbb713f16d0d64c0eabeb2
Zmingqin <zhangmingqinemail@163.com>
2016-08-24 12:09:35 +0800

    change js from pseduo to real

--- src/main/webapp/js/chineseStudy.js
+++ src/main/webapp/js/chineseStudy.js
@@ -2,43 +2,43 @@
  * Created by Administrator on 2016/8/23.
  */
 window.addEventListener('load', function (e) {
-    let g = {
-        title: "语文学习 | 三味学堂",
-        volumesL: [
-            {id: 1, name: 'xiao', title: 'hhhh'},
-            {id: 2, name: 'xiao', title: 'sdt'},
-            {id: 3, name: 'xiao', title: 'xxxx'},
-            {id: 4, name: 'xiao', title: 'jjjj'},
-            {id: 5, name: 'xiao', title: 'ddds'},
-            {id: 6, name: 'xiao', title: 'hhhh'},
-            {id: 7, name: 'xiao', title: 'vccvcv'},
-            {id: 8, name: 'xiao', title: 'ewr23'}
-        ],
-        volumesH: [
-            {id: 1, name: 'hight', title: 'hhhh'},
-            {id: 2, name: 'hight', title: 'sdt'},
-            {id: 3, name: 'hight', title: 'xxxx'},
-            {id: 4, name: 'hight', title: 'jjjj'},
-            {id: 5, name: 'hight', title: 'ddds'},
-            {id: 6, name: 'hight', title: 'hhhh'},
-            {id: 7, name: 'hight', title: 'vccvcv'},
-            {id: 8, name: 'hight', title: 'ewr23'}
-        ],
-        follows: {id: 1,title1:'title1', imgsrc: 'img/1.png', title2:'这是简介',step1:'昨天',step2:'xxxx',step3:'ffff'}
-    }
+    let subjects = []
+    // let followCount = 0
 
-    let data = {
-        title: "",
-        low: {
-        },
-        high: {
-            volumes: []
+    $.get('api/subjects', function(ss) {
+        subjects = ss
+        for (let i = 0; i < subjects.length; ++i) {
+            let filter = {
+                subjectId: subjects[i].id,
+                grade: 20
+            }
+            $.ajax({
+                type: "get",
+                url: 'api/volumes?filter=' + JSON.stringify(filter),
+                dataType: 'json',
+                async: false,
+                success: function(vs) {
+                    subjects[i].low = vs
+                }
+            })
+            filter.grade = 21
+            $.ajax({
+                type: "get",
+                url: 'api/volumes?filter=' + JSON.stringify(filter),
+                dataType: 'json',
+                async: false,
+                success: function(vs) {
+                    subjects[i].high = vs
+                }
+            })
         }
-    }
-
-    $.get('api/volumes', function(vs){
-        data.low.volumes = vs
     })
+
+    // $.get('api/follows', function(c) {
+    //     followCount = c
+    // })
+
+    let g = {}
 
     g.bind = function (element, data) {
         element.innerHTML = element.innerHTML.replace('%7B', '{').replace('%7D', '}').replace(/\$\{(\w+)\}/g, function (all, letiable) {
@@ -59,11 +59,12 @@ window.addEventListener('load', function (e) {
         });
         return element
     };
+
 //chineseStudy-title
     let title = document.getElementById('title-template');
     let atitle = document.getElementById('chineseStudy-title');
     let vtitle = title.content.children[0].cloneNode(true);
-    g.bind(vtitle, g);
+    g.bind(vtitle, subjects[0]);
     vtitle.addEventListener('click', function (e) {
         elm.style()
     }, false)
@@ -76,14 +77,14 @@ window.addEventListener('load', function (e) {
     let lvolumes = document.getElementById('chineseStudy-volumesL');
     for (let i = 0; i < g.volumesL.length; i++) {
         let vvolumes = volumes.content.children[0].cloneNode(true);
-        g.bind(vvolumes, g.volumesL[i]);
+        g.bind(vvolumes, subjects[0].low[i]);
         lvolumes.appendChild(vvolumes);
     }
     //hight
     let hvolumes = document.getElementById('chineseStudy-volumesH');
     for (let i = 0; i < g.volumesH.length; i++) {
         let vvolumes = volumes.content.children[0].cloneNode(true);
-        g.bind(vvolumes, g.volumesH[i]);
+        g.bind(vvolumes, subjects[0].high[i]);
         hvolumes.appendChild(vvolumes);
     }
     
@@ -98,11 +99,11 @@ window.addEventListener('load', function (e) {
         $('#chineseStudy-volumesH').show();
     })
 
-//follow
+    //follow
     let follow = document.getElementById('follow-template');
     let afollow = document.getElementById('follow');
     let vfollow = follow.content.children[0].cloneNode(true);
-    g.bind(vfollow, g.follows);
+    //g.bind(vfollow, followCount);
     afollow.appendChild(vfollow);
 
 
