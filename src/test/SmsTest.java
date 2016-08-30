6c7e13752f6a12c09a625962b6f2c0501c50a228
DuanShengpeng <duanshp@chineseall.com>
2016-08-30 17:03:56 +0800

    disable almost-all jax=rs package in pom.xml

--- src/test/SmsTest.java
+++ src/test/SmsTest.java
@@ -5,7 +5,7 @@ import com.baremind.Users;
  */
 public class SmsTest {
     public static void main(String[] args) {
-        Users.SendMessageResult r = Users.sendMessage("18810056729", "test valid code is 1432");
+        Users.SendMessageResult r = Users.sendMessage("18810056729", "second code is 1432");
         System.out.printf("time: %s, code: %s, id: %s\n", r.time, r.code, r.messageId);
     }
 }
