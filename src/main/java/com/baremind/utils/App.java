2bf31180b80d92c9093cbeb2eaab0ce09a1a146c
sunhonghe <15210038357@163.com>
2016-09-01 15:25:14 +0800

    to submit

--- src/main/java/com/baremind/utils/App.java
+++ src/main/java/com/baremind/utils/App.java
@@ -1,36 +1,19 @@
 package com.baremind.utils;
 import com.baremind.*;
+import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by User on 2016/9/1.
  */
-public class App extends Application {
-    @Override
-    public Set<Class<?>> getClasses() {
-        HashSet<Class<?>> classes = new HashSet<Class<?>>();
-        classes.add(Additionals.class);
-        classes.add(Cards.class);
-        classes.add(Comments.class);
-        classes.add(Devices.class);
-        classes.add(Images.class);
-        classes.add(KnowledgePoints.class);
-        classes.add(Likes.class);
-        classes.add(Medias.class);
-        classes.add(Problems.class);
-        classes.add(Properties.class);
-        classes.add(Schedulers.class);
-        classes.add(Sessions.class);
-        classes.add(Subjects.class);
-        classes.add(Tags.class);
-        classes.add(Testers.class);
-        classes.add(Users.class);
-        classes.add(Videos.class);
-        classes.add(WechatUsers.class);
-        classes.add(Volumes.class);
-        return classes;
-    }
+public class App extends ResourceConfig {
+    /*static {
+        registerClasses(Additionals.class, Cards.class, Comments.class, Devices.class,
+            Images.class, KnowledgePoints.class, Likes.class, Medias.class,
+            Problems.class, Properties.class, Schedulers.class, Sessions.class,
+            Subjects.class, Tags.class, Testers.class, Users.class, Videos.class,
+            Volumes.class, WechatUsers.class);
+    }*/
 }
