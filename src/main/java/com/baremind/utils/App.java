94ee9c61e92eed4f1446b0132be1acc11c5ba67e
sunhonghe <15210038357@163.com>
2016-09-01 15:41:47 +0800

    commit

--- src/main/java/com/baremind/utils/App.java
+++ src/main/java/com/baremind/utils/App.java
@@ -9,11 +9,11 @@ import java.util.Set;
  * Created by User on 2016/9/1.
  */
 public class App extends ResourceConfig {
-    /*static {
+    public App() {
         registerClasses(Additionals.class, Cards.class, Comments.class, Devices.class,
             Images.class, KnowledgePoints.class, Likes.class, Medias.class,
             Problems.class, Properties.class, Schedulers.class, Sessions.class,
             Subjects.class, Tags.class, Testers.class, Users.class, Videos.class,
             Volumes.class, WechatUsers.class);
-    }*/
+    }
 }
